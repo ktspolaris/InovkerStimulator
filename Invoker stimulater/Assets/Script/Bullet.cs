a129f1f71f333769c0241554c61b35c0af3ea352
Oliver Ye <wenzi1996@live.com>
2019-12-06 03:17:39 -0800

    Initial animation

---
+++ Invoker stimulater/Assets/Script/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public GameObject character;
+    protected  Transform b_transform;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b_transform = this.transform;
+        Destroy(this.gameObject, 1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        b_transform.Translate(new Vector2(0, speed * Time.deltaTime));
+    }
+}
---
+++ Invoker stimulater/Assets/Script/Character.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+
+   
+{
+    public float speed;
+    public float rotateSpeed;
+    public Transform bullet;
+    public GameObject shotSpawn;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rigidbody = this.GetComponent<Rigidbody>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+          this.transform.Rotate(new Vector3(0,0,1) * rotateSpeed * Time.deltaTime);
+            animator.SetBool("Iswalk", true);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+           this.transform.Rotate(new Vector3(0,0,1) * -rotateSpeed * Time.deltaTime);
+            
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector2(0,1) * speed * Time.deltaTime);
+            animator.SetBool("Iswalk", true);
+
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector2(0, 1) * -speed * Time.deltaTime);
+            animator.SetBool("Iswalk", true);
+
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            animator.SetBool("Iswalk", false);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetBool("Iswalk", false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetBool("Iswalk", false);
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            animator.SetBool("Iswalk", false);
+        }
+       
+
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Instantiate(bullet, shotSpawn.transform.position, shotSpawn.transform.rotation);
+            animator.SetBool("Isattack", true);
+        
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("Isattack", false);
+        }
+
+
+    }
+}
