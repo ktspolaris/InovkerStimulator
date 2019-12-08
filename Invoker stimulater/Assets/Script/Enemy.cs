ac0b62eec6e648a76f8acf8332fc14aaf73a1518
Oliver Ye <wenzi1996@live.com>
2019-12-07 18:22:33 -0800

    Enemy reaction

---
+++ Invoker stimulater/Assets/Script/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Collider2D collsion;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "FireBall" ){
+            animator.SetBool("IsBurned", true);
+            Destroy(this.gameObject, 2);
+        }
+
+        if (collision.gameObject.tag == "Flash")
+        {
+            animator.SetBool("IsZapped", true);
+            Destroy(this.gameObject, 2);
+        }
+
+        if (collision.gameObject.tag == "Wave")
+        {
+            animator.SetBool("IsWetted", true);
+            Destroy(this.gameObject, 2);
+        }
+        Destroy(collision.gameObject);
+    }
+}
